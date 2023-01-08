09567ddd451aea3f3df7b6f0011d1810e0f5f658
mvoloskov <hello@miloslav.website>
2023-01-08 18:35:07 +0400

    captions

--- gallery.mjs
+++ gallery.mjs
@@ -4,4 +4,23 @@ const lightbox = new PhotoSwipeLightbox({
 	children: 'a',
 	pswpModule: () => import('./photoswipe/photoswipe.esm.min.js'),
 })
+lightbox.on('uiRegister', function () {
+	lightbox.pswp.ui.registerElement({
+		name: 'custom-caption',
+		order: 9,
+		isButton: false,
+		appendTo: 'root',
+		html: 'Caption text',
+		onInit: (el, pswp) => {
+			lightbox.pswp.on('change', () => {
+				const currSlideElement = lightbox.pswp.currSlide.data.element
+				if (currSlideElement) {
+					el.innerHTML = currSlideElement.querySelector('img').getAttribute('alt')
+					const img = el.parentNode.querySelector('.pswp__img')
+					img.style.marginBottom = '32px'
+				}
+			})
+		},
+	})
+})
 lightbox.init()
