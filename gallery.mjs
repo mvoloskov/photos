f99ccc17746aa9ba5a88c2471e35b3b33b948e1f
mvoloskov <hello@miloslav.website>
2023-01-08 16:07:46 +0400

    aight let's do it

---
+++ gallery.mjs
@@ -0,0 +1,7 @@
+import PhotoSwipeLightbox from './photoswipe/photoswipe-lightbox.esm.min.js'
+const lightbox = new PhotoSwipeLightbox({
+	gallery: '.gallery',
+	children: 'a',
+	pswpModule: () => import('./photoswipe/photoswipe.esm.min.js'),
+})
+lightbox.init()
